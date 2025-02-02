616a73acd8c58dbc9cdd1f7aa60f4074e10b13ae
unknown <talyamalka2000@gmail.com>
2025-02-02 16:39:26 +0200

    writed main to check if the function writesToFile is good

--- ThreadsFilesP1/main.cpp
+++ ThreadsFilesP1/main.cpp
@@ -5,11 +5,20 @@ int main()
 {
     int begin = 1;
     int end = 100;
-    std::string filePath = "primes.txt";
+    std::string filePath = "C:\\Users\\Cyber_User\\Desktop\\EkronotExercises\\ThreadsSync\\primes.txt";
     int N = 4;
 
-    callWritePrimesMultipleThreads(begin, end, filePath, N);
+    // Open file for writing
+    std::ofstream file(filePath);
+    if (!file.is_open())
+    {
+        std::cerr << "Error opening file: " << filePath << std::endl;
+        return 1;
+    }
 
-    std::cout << "Finished writing primes to file: " << filePath << std::endl;
+    writePrimesToFile(begin, end, file);
+    //callWritePrimesMultipleThreads(begin, end, filePath, N);
+
+    //std::cout << "Finished writing primes to file: " << filePath << std::endl;
     return 0;
 }
